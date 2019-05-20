77c2741a07913f4ad5ccd92af3a10e1730e0bdc7
Craig Mullin <cmullin@dreeshomes.com>
2019-05-20 16:35:34 -0400

    updates

--- scripts/my.js
+++ scripts/my.js
@@ -1,22 +1,18 @@
-// $(function() {
-//   var randomColor = Math.floor(Math.random() * 16777215).toString(16)
+$(function cColor() {
+  console.log('cColor function')
+  var colorArray = ['red', 'orange', 'green', 'blue', 'purple', 'limegreen', 'deepskyblue', 'navy', 'slategray', 'orangered', 'darkred', 'darkorange']
+  var randomColor = colorArray[Math.floor(Math.random() * colorArray.length)]
+  console.log(Math.floor(Math.random() * colorArray.length))
+  console.log('new test')
 
-//   $('body').css({
-//     backgroundColor: '#' + randomColor,
-//   })
+  $('#background-text').css({
+    //color: randomColor,
+    // color: '#ffcb35',
+    color: 'orange',
+  })
+})
 
-//   $('#colorcode').text('#' + randomColor)
+// $(window).scroll(function() {
+//   var theta = ($(window).scrollTop() / 500) % Math.PI
+//   $('#background-text').css({ transform: 'rotate(-' + theta + 'rad)' })
 // })
-$(
-  function() {
-    testFunction()
-  },
-
-  function testFunction() {
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16)
-
-    $('#background-text').css({
-      color: '#' + randomColor,
-    })
-  }
-)
